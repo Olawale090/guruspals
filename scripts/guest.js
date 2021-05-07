3fad25761dc7cc658255e15c42c5ab54e918fef0
vickyFabrics <mosesolawale21@gmail.com>
2021-05-07 09:44:19 +0100

    navigator vibration attached to forms

--- scripts/guest.js
+++ scripts/guest.js
@@ -7,36 +7,55 @@ const guestAccount = function(){
     this.examType = document.querySelector(".guest_exam_type");
     this.guestLogin = document.querySelector(".guest_login");
     this.data = [2345,7454,1523,8463,1637];
-
+    this.alert = document.querySelector(".form_notification");
 
 };
 
 guestAccount.prototype = {
 
     verifyGuestData(){
-        this.guestLogin.addEventListener('click',()=>{
+        
+        try {
+            
+            this.guestLogin.addEventListener('click',(e)=>{
+                e.preventDefault();
 
-            let phoneNo = Number(this.phoneNumber.value);
-            let mobileResponse = navigator.vibrate([200,50,400]);
-            if(this.phoneNumber.value === "" || this.password.value === "" || this.examType === ""){
+                try {
                     
-                alert(" Please enter value in the empty field ");
-                mobileResponse
-            }else if(typeof(phoneNo) !== Number){
+                    let phoneNo = Number(this.phoneNumber.value);
+                    let mobileResponse = navigator.vibrate([200,50,400]);
+                    if(this.phoneNumber.value === "" || this.password.value === "" || this.examType === ""){
+                            
+                        this.alert.textContent = " Please enter value in the empty field ";
+                        mobileResponse
+                    }else if(typeof(phoneNo) !== Number){
+        
+                        this.alert.textContent = "Enter correct Phone number" ;
+                        mobileResponse
+                    }else if(this.data.includes(phoneNo)){
+        
+                        this.alert.textContent = "You're logged in" ;
+                        this.alert.style.color = "blue";
+                        
+                    }else{
+        
+                        alert("Please try again");
+                        mobileResponse
+                    }
+        
+              
 
-                alert("Enter correct Phone number");
-                mobileResponse
-            }else if(this.data.includes(phoneNo)){
-
-                alert("You're logged in");
+                } catch (error) {
+                    console.error("Error occurred while rendering event", error);
+                }  
+            
+            });
                 
-            }else{
 
-                alert("Please try again");
-                mobileResponse
-            }
-
-        });
+        } catch (error) {
+            console.error("Error in executing method verifyGuestData ", error);
+        }
+        
         
     }
 
